56817c0050fdeda787e7b948c95ebb86d4f597d5
1193662 <p.soumya@gmail.com>
2023-09-28 09:02:13 +0530

    message

---
+++ empservice/src/test/java/empservice/empservice/EmpserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package empservice.empservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
